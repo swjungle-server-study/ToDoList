d6b985fe97f0c37285fb6e267cb7bd67e1b00249
seonghoo1217 <lsh6451217@gmail.com>
2024-06-02 02:17:01 +0900

    feat: 투두 필수 정보 수정(patch) API Controller 연결

--- todo/src/main/java/jungle/study/todo/presentation/ToDoController.java
+++ todo/src/main/java/jungle/study/todo/presentation/ToDoController.java
@@ -5,6 +5,7 @@ import jungle.study.todo.application.service.ToDoQueryService;
 import jungle.study.todo.domain.ToDo;
 import jungle.study.todo.global.dto.ResponseEnvelope;
 import jungle.study.todo.presentation.dto.request.CreateToDoReq;
+import jungle.study.todo.presentation.dto.request.ModifyToDoReq;
 import jungle.study.todo.presentation.dto.response.ToDoUuidRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -37,5 +38,11 @@ public class ToDoController {
     public ResponseEnvelope<?> findAllTodos() {
         List<ToDo> todos = toDoQueryService.findAllToDo();
         return ResponseEnvelope.of(todos);
+    }
+
+    @PatchMapping
+    public ResponseEnvelope<?> modifyToDoEssential(@RequestBody ModifyToDoReq modifyToDoReq) {
+        ToDo toDo = toDoCommandService.modifyToDoEssential(modifyToDoReq);
+        return ResponseEnvelope.of(toDo);
     }
 }
