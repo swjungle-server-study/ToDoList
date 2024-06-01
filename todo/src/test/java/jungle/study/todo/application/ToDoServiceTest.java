57c4f3e3a0c3cf5a266fdc8d31cc9cacfc4cd4b0
seonghoo1217 <lsh6451217@gmail.com>
2024-06-01 15:07:31 +0900

    test: 투두 단건 조회에 대한 API 테스트 코드 작성

--- todo/src/test/java/jungle/study/todo/application/ToDoServiceTest.java
+++ todo/src/test/java/jungle/study/todo/application/ToDoServiceTest.java
@@ -2,18 +2,25 @@ package jungle.study.todo.application;
 
 
 import jungle.study.todo.application.service.ToDoCommandService;
+import jungle.study.todo.application.service.ToDoQueryService;
 import jungle.study.todo.domain.Category;
 import jungle.study.todo.domain.ToDo;
 import jungle.study.todo.domain.ToDoEssential;
+import jungle.study.todo.domain.exception.ToDoNotFoundException;
 import jungle.study.todo.domain.repository.ToDoRepository;
+import jungle.study.todo.presentation.dto.request.TodoDetailReq;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -27,6 +34,11 @@ public class ToDoServiceTest {
     @Autowired
     ToDoCommandService toDoCommandService;
 
+    @Autowired
+    ToDoQueryService toDoQueryService;
+
+    String NOT_FOUND = "투두가 존재하지 않습니다";
+
     @Test
     @DisplayName("투두 단건 생성 API DB 연결")
     public void createTodoConnectionOnRepository(){
@@ -38,6 +50,19 @@ public class ToDoServiceTest {
         // then
         assertNotNull(createdToDo);
         assertEquals(2, toDoRepository.todoSeq.get());
+    }
+
+    @Test
+    @DisplayName("투두 단건 조회 API")
+    public void findTodoByUuId(){
+        //given
+        ToDo toDo = basicToDoResponse();
+        ToDo createToDo = toDoRepository.createToDo(toDo);
+        TodoDetailReq todoDetailReq = new TodoDetailReq(createToDo.getUuid());
+        //when
+        ToDo todo = toDoQueryService.findTodoByUuid(todoDetailReq);
+        //then
+        assertThat(todo.getUuid()).isEqualTo(createToDo.getUuid());
     }
 
     private ToDo basicToDoResponse(){
