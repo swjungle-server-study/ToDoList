8f15a9e433c814a38cb4526bb96a45067a1cf8eb
seonghoo1217 <lsh6451217@gmail.com>
2024-06-01 16:40:56 +0900

    fix: AtomicInteger AutoIncrement Key 버그 이슈

--- todo/src/main/java/jungle/study/todo/domain/repository/ToDoRepository.java
+++ todo/src/main/java/jungle/study/todo/domain/repository/ToDoRepository.java
@@ -6,18 +6,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class ToDoRepository {
     public AtomicInteger todoSeq = new AtomicInteger(1);
-    ConcurrentHashMap<AtomicInteger,ToDo> todoDB = new ConcurrentHashMap<>();
+    ConcurrentHashMap<Integer,ToDo> todoDB = new ConcurrentHashMap<>();
     public ToDo createToDo(ToDo toDo){
-        AtomicInteger currentSeq = todoSeq;
-        todoDB.put(todoSeq,toDo);
+        Integer currentSeq = todoSeq.get();
+
+        todoDB.put(currentSeq,toDo);
 
         todoSeq.incrementAndGet();
         return todoDB.get(currentSeq);
