6cb4944065ddd3fd920a2fdbd1b20147130e3c76
seonghoo1217 <lsh6451217@gmail.com>
2024-06-03 17:30:26 +0900

    feat: Category Enum 값 ToString 메서드 정의

--- todo/src/main/java/jungle/study/todo/domain/Category.java
+++ todo/src/main/java/jungle/study/todo/domain/Category.java
@@ -1,5 +1,18 @@
 package jungle.study.todo.domain;
 
 public enum Category {
-    TODO,DOING,DONE
+    TODO("ToDo"),
+    DOING("Doing"),
+    DONE("Done");
+
+    private final String status;
+
+    Category(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return this.status;
+    }
 }
