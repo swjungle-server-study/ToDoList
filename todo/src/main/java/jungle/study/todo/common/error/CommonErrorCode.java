bac456285e4029d34a8141198ae3d98f1086fba3
Cheongwoo Nam <skacjddn7@gmail.com>
2024-06-03 15:27:09 +0900

    feat : 봉투 패턴(error, data, message) 적용

---
+++ todo/src/main/java/jungle/study/todo/common/error/CommonErrorCode.java
@@ -0,0 +1,20 @@
+package jungle.study.todo.common.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum CommonErrorCode implements ErrorCode {
+
+    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid parameter included"),
+    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "Resource not exists"),
+    TODO_NOT_FOUND(HttpStatus.INTERNAL_SERVER_ERROR, "Find"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
