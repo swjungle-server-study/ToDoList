39ec651cd91b079e51c6791d9fd089d8f3bdacd4
dylanYim <dongkyun94@gmail.com>
2024-06-03 20:48:14 +0900

    config : add swagger

---
+++ todo/src/main/java/jungle/study/todo/api/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package jungle.study.todo.api.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Todo API")
+                .description("Swagger for controller test")
+                .version("0.1.0"); // properties에서 읽어 오는 걸로 변경 예정
+    }
+}
