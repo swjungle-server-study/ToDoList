d2a584a67941f26b440a77a8700984f7165d1980
Cheongwoo Nam <skacjddn7@gmail.com>
2024-06-05 16:58:42 +0900

    feat: JPA 기반 Entity 변경

--- todo/src/main/java/jungle/study/todo/domain/Todo.java
+++ todo/src/main/java/jungle/study/todo/domain/Todo.java
@@ -1,14 +1,28 @@
 package jungle.study.todo.domain;
 
+import jakarta.persistence.*;
+import jakarta.persistence.GenerationType;
 import jungle.study.todo.dto.TodoRequestDto;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity // JPA 엔티티임을 나타냄
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA는 기본 생성자가 필요합니다.
 @AllArgsConstructor
 public class Todo {
+    @Id // 기본 키
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 증가 ID 생성 전략
     private Long id;
+
+    @Column(nullable = false) // null 허용하지 않음
     private String title;
+
+    @Column(nullable = false)
     private String content;
+
     private boolean completed;
 
     public static Todo of(Long id, String title, String content, boolean completed) {
